d32908138a6f31e93364eeefab5ce0afe4e2885f
Subrata7a <subrata@appscode.com>
2024-02-14 18:25:57 +0600

    Third

--- model/book.go
+++ model/book.go
@@ -1,9 +1,9 @@
 package model
 
 type Book struct {
-	UUID        string   `json:"UUID"`
+	UUID        string   `json:"uuid"`
 	Name        string   `json:"name"`
 	AuthorList  []string `json:"authorList"`
 	PublishDate string   `json:"publishDate"`
-	ISBN        string   `json:"ISBN"`
+	ISBN        string   `json:"isbn"`
 }
